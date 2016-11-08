966661bb14901612c44b0eb9718be6ea9f6ef446
Andrei Cristian Petcu <andrei@ceata.org>
2016-11-08 21:50:25 +0200

    addded request text from old template

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Add a callback - to be executed before each request in development,
 # and at startup in production - to patch existing app classes.
 # Doing so in init/environment.rb wouldn't work in development, since
@@ -9,7 +10,15 @@ Rails.configuration.to_prepare do
         # Add intro paragraph to new request template
         def default_letter
             return nil if self.message_type == 'followup'
-            #"If you uncomment this line, this text will appear as default text in every message"    
+            "prin grija compartimentului de informare şi relaţii publice:
+
+Nu vă supăraţi,
+Conform Legii nr. 544/2001 privind liberul acces la informaţiile de interes public, doresc să primesc câte o copie a documentelor oficiale care conţin următoarele informaţii:
+
+
+
+
+Sunt dispus să plătesc taxele aferente serviciilor de copiere pentru documentele solicitate (dacă solicitaţi copii imprimate pe suport fizic, de hârtie, de exemplu--în acest caz, nu uitaţi să completaţi adresa de corespondenţă sub semnatură)."
         end
-    end        
+    end
 end
